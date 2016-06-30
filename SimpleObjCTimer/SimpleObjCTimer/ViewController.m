b24b42a84911417873476250d9d20e43ac6f3da1
Stan Hughes <Stanley.Hughes@AcuityBrands.com>
2016-06-30 18:25:22 -0400

    Initial generated app.

---
+++ SimpleObjCTimer/SimpleObjCTimer/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimpleObjCTimer
+//
+//  Created by Stan Hughes on 6/30/16.
+//  Copyright © 2016 Stan Hughes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
